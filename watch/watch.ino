9cfb36fb66d11c518f12a66c75f2c04a82024d54
cesare.micheletto.takeo.bruno@gmail.com <cybersqeak@yahoo.com>
2026-06-05 17:57:10 +0900

    I manage to install DHT11 the problem i had when trying to install DHT11 yesterday was  that my library wasn't compatible  to esp32 . I also found that my DHT11 run on 3.3v so I had to seperate circuit with other modules  such as TM1673. my plan is to make other button to have option whether I display time or humidity and temperature.
    it occurd time display error for some reason.
    it collects minuite correctly but hour wrongly...
    i need to figure out what's going on there....
    that is my next goal.
    
    goodbye.

--- watch/watch.ino
+++ watch/watch.ino
@@ -3,52 +3,21 @@
 #include "RTClib.h"
 #include <TM1637Display.h>
 #include <stdio.h>
-#include <DHT11.h>
+#include <DHT.h>
 RTC_DS3231 rtc;
 
 #define CLK 18
 #define DIO 19
 #define ALERT 2
 #define DB_PIN 4 
-#define DHT11PIN 5
+#define DHTB_PIN 13
+#define DHTPIN 5
+#define DHTTYPE DHT11
 
 TM1637Display display(CLK, DIO);
-DHT11 dht11(DHT11PIN);  
 
-void checkdht(int humidity, int temperature)
-{
-    if (humidity == DHT11::ERROR_CHECKSUM ||
-        humidity == DHT11::ERROR_TIMEOUT ||
-        temperature == DHT11::ERROR_CHECKSUM ||
-        temperature == DHT11::ERROR_TIMEOUT)
-    {
-        Serial.println("DHT read failed");
-        return;
-    }
+DHT dht(DHTPIN, DHTTYPE);  
 
-    Serial.print("humidity: ");
-    Serial.println(humidity);
-
-    Serial.print("temperature: ");
-    Serial.println(temperature);
-}
-/*
-void    checkdht(int humidity, int temperature)
-{
-    if (isnan(humidity) || isnan(temperature))
-  {
-      Serial.println("Failed to read from  dht sensor");
-      return ;
-  }
-  Serial.println("humidity: ");
-  Serial.println(humidity);
-  Serial.println(" %");
-  Serial.println("temperature: ");
-  Serial.println(temperature);
-  Serial.println(" *C");
-}
-  
-*/
 int check_button_state(int button_pin, int flag)
 {
     if (button_pin == 0 && flag == 1) 
@@ -92,42 +61,68 @@ int alert_sound(int pin)
 }    
 
 int db_state;
+int dht_button_state;
 int laststate = 1;
+int dht_button_laststate = 1;
 void setup() {
+  Serial.begin(115200);
   Wire.begin(21, 22);
   rtc.begin();
-//  dht.begin();
-  Serial.begin(115200);
+  dht.begin();
+
   pinMode(ALERT,OUTPUT);
   pinMode(DB_PIN,INPUT_PULLUP);
+  pinMode(DHTB_PIN,INPUT_PULLUP);
   analogWrite(ALERT,0);
+
   delay(2000);
 }
+
+void checkdht(float h, float t)
+{
+    Serial.println("current humidity % Temperature:");
+    Serial.println(h);
+    Serial.println("% ");
+    Serial.println(t);
+    Serial.println("C ");
+}
+
+
 void loop() {
   DateTime now = rtc.now();
+
   int h = now.hour();
   int m = now.minute();
- int hum = dht11.readHumidity();
-  int  temp = dht11.readTemperature();
-  checkdht(hum,temp);
+
+  float  hum = dht.readHumidity();
+  float  temp = dht.readTemperature();
+  
+//  checkdht(hum,temp);
   db_state = digitalRead(DB_PIN);
+  dht_button_state = digitalRead(DHTB_PIN);
 
   // HHMM dis // 
   int value = h * 100 + m;
   display.showNumberDecEx(value, 0b01000000, true);
 
   laststate = check_button_state(db_state,laststate);
+  dht_button_laststate = check_button_state(dht_button_state,dht_button_laststate);
+
+
   if (check_alert(h,m))
       {
           laststate = check_button_state(0,0);
           alert_sound(ALERT);
       }
-
-      // i want to 
+//  Serial.println("the DHTPIN is");
+ // Serial.println(dht_button_state);
+  //Serial.println("the laststate is");
+  //Serial.println(laststate);
+  // i want to 
 //  display.showNumberDecEx(temp, 0b01000000, true);
 //  printf("the current time is :%d\n",value);
 //    printf("the db_state and laststate is  %d : %d\n",db_state, laststate);
-  delay(1000);
+  delay(10);
 }
 
 
