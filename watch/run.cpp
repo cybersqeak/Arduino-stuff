81836f8bed18c45d6c411aead532aa0fbaf2939d
cesare.micheletto.takeo.bruno@gmail.com <cybersqeak@yahoo.com>
2026-08-25 17:30:21 +0900

    humidity mark identified!
    it was 118 in integer
    
    by using it, Now successfully display H (for ex,39H)
    
    great!  next thing i want is the alert time!
    
    goodbye from sqeak

--- watch/run.cpp
+++ watch/run.cpp
@@ -18,17 +18,13 @@ static void display_humid(void)
 {
     int hum = dht.readHumidity();
     uint8_t seg_data[4];
-    for (int i = 0;i < 100;i++)
-    {
-    seg_data[0] = display.encodeDigit(hum / 10);
-    seg_data[1] = display.encodeDigit(hum % 10);
-    seg_data[2] = i;                               // H for humidity (no real % glyph exists)
-    seg_data[3] = SEG_u;                                  // blank
-    Serial.printf("i is %d",i);
+    seg_data[0] = 0;                                  // blank
+    seg_data[1] = display.encodeDigit(hum / 10);
+    seg_data[2] = display.encodeDigit(hum % 10);
+    seg_data[3] = 118;                               // H for humidity (no real % glyph exists)
     display.setSegments(seg_data);
     display.setBrightness(7, true);
-    delay(1000);
-    }
+    
 }
 static void dis(void)
 {
